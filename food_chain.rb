86050f2f80adaf351e6e90f339a558c9d8a072db
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-25 00:12:10 +0800

    Code cleanup. Remove DEBUG statement.

--- food_chain.rb
+++ food_chain.rb
@@ -25,26 +25,26 @@ class FoodChain
     for i in 1..menagerie.count
       animal = menagerie[i - 1]
 
-      puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal}.     #{i}"      
+      puts "I know an old lady who swallowed a #{animal}.     #{i}"      
 
       case animal
       when "fly"
         # Do nothing. This is here to make sure 'fly' is processed and not mistakenly be unrecognized...
       when "bird" 
-        puts "@DEBUG #{__LINE__}    How absurd to swallow a #{animal}!" 
+        puts "How absurd to swallow a #{animal}!" 
       when "cat" 
-        puts "@DEBUG #{__LINE__}    Imagine that, to swallow a #{animal}!"
+        puts "Imagine that, to swallow a #{animal}!"
       when "dog"
-        puts "@DEBUG #{__LINE__}    What a hog, to swallow a #{animal}!"
+        puts "What a hog, to swallow a #{animal}!"
       when  "goat" 
-        puts "@DEBUG #{__LINE__}    Just opened her throat and swallowed a #{animal}!"
+        puts "Just opened her throat and swallowed a #{animal}!"
       when  "cow" 
-        puts "@DEBUG #{__LINE__}    I don't know how she swallowed a #{animal}!"
+        puts "I don't know how she swallowed a #{animal}!"
       when "horse" 
-        puts "@DEBUG #{__LINE__}    She's dead, of course!"
+        puts "She's dead, of course!"
         break 
       else
-        puts "@DEBUG #{__LINE__}    ERROR! Unrecognized animal, #{animal}." 
+        puts "ERROR! Unrecognized animal, #{animal}." 
       end    
       
 
@@ -56,19 +56,17 @@ class FoodChain
     
         case previously_swallowed
         when "goat", "dog", "cat", "bird", "fly"
-          puts "@DEBUG #{__LINE__}    She swallowed the #{currently_swallowed} to catch the #{previously_swallowed}."
+          puts "She swallowed the #{currently_swallowed} to catch the #{previously_swallowed}."
         when "spider"
-          puts "@DEBUG #{__LINE__}    She swallowed the #{currently_swallowed} to catch the #{previously_swallowed} that wriggled and jiggled and tickled inside her."
+          puts "She swallowed the #{currently_swallowed} to catch the #{previously_swallowed} that wriggled and jiggled and tickled inside her."
         else
-          puts "@DEBUG #{__LINE__}    ERROR! Unrecognized animal, #{previously_swallowed}."    
+          puts "ERROR! Unrecognized animal, #{previously_swallowed}."    
         end
       end
       last_swallowed = previously_swallowed
       
       
-      puts "@DEBUG #{__LINE__}    I don't know why she swallowed the #{last_swallowed}. Perhaps she'll die."
-      puts "@DEBUG #{__LINE__}    "
-      puts "@DEBUG #{__LINE__}    "
+      puts "I don't know why she swallowed the #{last_swallowed}. Perhaps she'll die.\n\n\n"
     end
   end  
   
