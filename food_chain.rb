9b05f8193301214b60ae8fdb1f1adde0ab9eccf5
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-24 21:45:54 +0800

    Coded the initial itiration to figure out the pattern. Added food_chain_song.text.

--- food_chain.rb
+++ food_chain.rb
@@ -8,11 +8,31 @@ class FoodChain
   end
   
   def initialize
-    puts "@DEBUG #{__LINE__}    Running inside initialize..."
   end  
   
   def main
     puts "@DEBUG #{__LINE__}    Running inside main..."
+    
+    animal_menagerie = [
+      "fly",
+      "spider",
+      "bird",
+      "cat",
+      "dog",
+      "goat",
+      "cow",
+      "horse",
+    ]
+
+    for i in 0..(animal_menagerie.size - 1) 
+      puts "@DEBUG #{__LINE__}    #{animal_menagerie[i]}"
+      (i - 1).downto(0) do |n|
+        puts "@DEBUG #{__LINE__}    #{animal_menagerie[n]}"
+      end
+      
+      
+      puts "@DEBUG #{__LINE__}    "
+    end
   end  
   
 end
