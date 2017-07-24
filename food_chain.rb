b466cd89639fd2a5c749e266c060241e84a64dbe
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-25 00:09:24 +0800

    Code refactor. Change the if-elsif statement to case-when statement.

--- food_chain.rb
+++ food_chain.rb
@@ -25,33 +25,31 @@ class FoodChain
     for i in 1..menagerie.count
       animal = menagerie[i - 1]
 
-      puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{menagerie[i - 1]}.     #{i}"      
+      puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal}.     #{i}"      
 
-      if    animal == "horse"
-        puts "@DEBUG #{__LINE__}    he's dead, of course!"
-        break
+      case animal
+      when "fly"
+        # Do nothing. This is here to make sure 'fly' is processed and not mistakenly be unrecognized...
+      when "bird" 
+        puts "@DEBUG #{__LINE__}    How absurd to swallow a #{animal}!" 
+      when "cat" 
+        puts "@DEBUG #{__LINE__}    Imagine that, to swallow a #{animal}!"
+      when "dog"
+        puts "@DEBUG #{__LINE__}    What a hog, to swallow a #{animal}!"
+      when  "goat" 
+        puts "@DEBUG #{__LINE__}    Just opened her throat and swallowed a #{animal}!"
+      when  "cow" 
+        puts "@DEBUG #{__LINE__}    I don't know how she swallowed a #{animal}!"
+      when "horse" 
+        puts "@DEBUG #{__LINE__}    She's dead, of course!"
+        break 
+      else
+        puts "@DEBUG #{__LINE__}    ERROR! Unrecognized animal, #{animal}." 
+      end    
       
-      elsif animal == "cow"
-        puts "@DEBUG #{__LINE__}    I don't know how she swallowed a #{menagerie[i - 1]}!"
-        
-      elsif animal == "goat"
-        puts "@DEBUG #{__LINE__}    Just opened her throat and swallowed a #{menagerie[i - 1]}!"
-      
-      elsif animal == "dog"
-        puts "@DEBUG #{__LINE__}    What a hog, to swallow a #{menagerie[i - 1]}!"
-        
-      elsif animal == "cat"
-        puts "@DEBUG #{__LINE__}    Imagine that, to swallow a #{menagerie[i - 1]}!"
-        
-      elsif animal == "bird"
-        puts "@DEBUG #{__LINE__}    How absurd to swallow a bird!" 
-        
-      elsif animal == "spider"
-        puts "@DEBUG #{__LINE__}    It wriggled and jiggled and tickled inside her." 
-      end  
 
       previously_swallowed = nil
-      previously_swallowed = nil
+      currently_swallowed  = nil
       (i - 1).downto(1) do |n|
         previously_swallowed = menagerie[n - 1]
          currently_swallowed = menagerie[n    ]
@@ -62,14 +60,13 @@ class FoodChain
         when "spider"
           puts "@DEBUG #{__LINE__}    She swallowed the #{currently_swallowed} to catch the #{previously_swallowed} that wriggled and jiggled and tickled inside her."
         else
-          puts "@DEBUG #{__LINE__}    Error! Unrecognized animal, #{previously_swallowed}."    
+          puts "@DEBUG #{__LINE__}    ERROR! Unrecognized animal, #{previously_swallowed}."    
         end
       end
       last_swallowed = previously_swallowed
       
       
       puts "@DEBUG #{__LINE__}    I don't know why she swallowed the #{last_swallowed}. Perhaps she'll die."
-      puts "@DEBUG #{__LINE__}    "
       puts "@DEBUG #{__LINE__}    "
       puts "@DEBUG #{__LINE__}    "
     end
