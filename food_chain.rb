0036ccda599cb4ae408917c51b751e3f9698c423
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-24 23:51:50 +0800

    Code refactor. Simplify duplicated code.

--- food_chain.rb
+++ food_chain.rb
@@ -11,7 +11,7 @@ class FoodChain
   end  
   
   def main
-    animal_menagerie = [
+    menagerie = [
       "fly",
       "spider",
       "bird",
@@ -22,47 +22,39 @@ class FoodChain
       "horse",
     ]
 
-    for i in 1..animal_menagerie.count
-      animal = animal_menagerie[i - 1]
-      
+    for i in 1..menagerie.count
+      animal = menagerie[i - 1]
+
+      puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{menagerie[i - 1]}.     #{i}"      
+
       if    animal == "horse"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
         puts "@DEBUG #{__LINE__}    he's dead, of course!"
         break
       
       elsif animal == "cow"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
-        puts "@DEBUG #{__LINE__}    I don't know how she swallowed a #{animal_menagerie[i - 1]}!"
+        puts "@DEBUG #{__LINE__}    I don't know how she swallowed a #{menagerie[i - 1]}!"
         
       elsif animal == "goat"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
-        puts "@DEBUG #{__LINE__}    Just opened her throat and swallowed a #{animal_menagerie[i - 1]}!"
+        puts "@DEBUG #{__LINE__}    Just opened her throat and swallowed a #{menagerie[i - 1]}!"
       
       elsif animal == "dog"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
-        puts "@DEBUG #{__LINE__}    What a hog, to swallow a #{animal_menagerie[i - 1]}!"
+        puts "@DEBUG #{__LINE__}    What a hog, to swallow a #{menagerie[i - 1]}!"
         
       elsif animal == "cat"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
-        puts "@DEBUG #{__LINE__}    Imagine that, to swallow a #{animal_menagerie[i - 1]}!"
+        puts "@DEBUG #{__LINE__}    Imagine that, to swallow a #{menagerie[i - 1]}!"
         
       elsif animal == "bird"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
         puts "@DEBUG #{__LINE__}    How absurd to swallow a bird!" 
         
       elsif animal == "spider"
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
         puts "@DEBUG #{__LINE__}    It wriggled and jiggled and tickled inside her." 
-        
-      elsif animal == "fly"  
-        puts "@DEBUG #{__LINE__}    I know an old lady who swallowed a #{animal_menagerie[i - 1]}.     #{i}"
       end  
 
       previously_swallowed = nil
       previously_swallowed = nil
       (i - 1).downto(1) do |n|
-        previously_swallowed = animal_menagerie[n - 1]
-         currently_swallowed = animal_menagerie[n    ]
+        previously_swallowed = menagerie[n - 1]
+         currently_swallowed = menagerie[n    ]
     
         case previously_swallowed
         when "goat", "dog", "cat", "bird", "fly"
