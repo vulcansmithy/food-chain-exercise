c18eb1c58fac1ad0fb5f8a87ccf2abe12c1c116f
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-24 21:08:58 +0800

    Added a hook so that the main method would be executed if the food_chain.rb is executed from the command line.

---
+++ food_chain.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + "/food_chain.rb"
+
+class FoodChain
+  
+  # this enable this ruby script to be run from the command line. Example - $> ruby food_chain.rb
+  if __FILE__ == $PROGRAM_NAME
+    FoodChain.new.main 
+  end
+  
+  def main
+    puts "@DEBUG #{__LINE__}    Running inside main..."
+  end  
+  
+end
