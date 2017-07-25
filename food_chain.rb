701ec26db4cde62c2fe0741eefc8a8aac4865ba1
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-07-25 11:04:42 +0800

    Further refactor and code cleanup.

--- food_chain.rb
+++ food_chain.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + "/food_chain.rb"
 
 class FoodChain
   
-  # this enable this ruby script to be run from the command line. Example - $> ruby food_chain.rb
+  # this enable this ruby script to be run from the command output. Example - $> ruby food_chain.rb
   if __FILE__ == $PROGRAM_NAME
     FoodChain.new.main 
   end
@@ -26,9 +26,10 @@ class FoodChain
     for i in 1..@menagerie.size
       picked_creature = @menagerie[i - 1]
 
-      build_first_line_per_verse( picked_creature)
-      build_second_line_per_verse(picked_creature)    
+      build_first_output_per_verse( picked_creature)
+      build_second_output_per_verse(picked_creature)    
 
+      # if the picked creature is a 'horse' just skip the code below and display the last output in the verse
       unless picked_creature == "horse"
         
         last_swallowed_creature = (i == 1 ? "fly" : nil)
@@ -39,7 +40,7 @@ class FoodChain
           last_swallowed_creature       = build_who_swallowed_who(currently_swallowed_creature, previously_swallowed_creature)        
         end
         
-        build_last_line_per_verse(last_swallowed_creature)
+        build_last_output_per_verse(last_swallowed_creature)
       end  
     end
     
@@ -48,15 +49,20 @@ class FoodChain
     return @song_lyrics
   end  
   
-  def build_first_line_per_verse(picked_creature)
-    line = "I know an old lady who swallowed a #{picked_creature}.\n"  
+  def build_first_output_per_verse(picked_creature)
+    if picked_creature.nil? or picked_creature.empty?
+      raise "Passed 'picked_creature' was either nil or empty string. Said parameter should not be nil or empty." 
+    end
     
-    @song_lyrics += line
+    @song_lyrics += "I know an old lady who swallowed a #{picked_creature}.\n" 
   end
   
-  def build_second_line_per_verse(picked_creature)
+  def build_second_output_per_verse(picked_creature)
+    if picked_creature.nil? or picked_creature.empty?
+      raise "Passed 'picked_creature' was either nil or empty string. Said parameter should not be nil or empty." 
+    end
  
-    line = case picked_creature
+    output = case picked_creature
     when "fly"
       "" # Intentionally left blank. This is here to make sure 'fly' is processed and not mistakenly to be "unrecognized"...
     when "spider"
@@ -74,35 +80,46 @@ class FoodChain
     when "horse" 
       "She's dead, of course!"
     else
-      puts "ERROR! Unrecognized submitted creature, #{picked_creature}." 
-      return
+      raise "Unrecognized submitted creature. '#{picked_creature}'." 
     end  
  
-    @song_lyrics += line
+    @song_lyrics += output
   end
   
-  def build_last_line_per_verse(last_swallowed_creature)
+  def build_last_output_per_verse(last_swallowed_creature)
+    if last_swallowed_creature.nil? or last_swallowed_creature.empty?
+      raise "Passed 'last_swallowed_creature' was either nil or empty string. Said parameter should not be nil or empty." 
+    end
+    
     @song_lyrics += "I don't know why she swallowed the #{last_swallowed_creature}. Perhaps she'll die.\n\n"
   end
   
   def build_who_swallowed_who(currently_swallowed_creature, previously_swallowed_creature)
+    if currently_swallowed_creature.nil? or currently_swallowed_creature.empty?
+      raise "Passed 'currently_swallowed_creature' was either nil or empty string. Said parameter should not be nil or empty." 
+    end
     
-    line = case previously_swallowed_creature
+    if previously_swallowed_creature.nil? or previously_swallowed_creature.empty?
+      raise "Passed 'previously_swallowed_creature' was either nil or empty string. Said parameter should not be nil or empty." 
+    end
+      
+    
+    output = case previously_swallowed_creature
     when "goat", "dog", "cat", "bird", "fly"
       "She swallowed the #{currently_swallowed_creature} to catch the #{previously_swallowed_creature}.\n"
     when "spider"
       "She swallowed the #{currently_swallowed_creature} to catch the #{previously_swallowed_creature} that wriggled and jiggled and tickled inside her.\n"
     else
-      "ERROR! Unrecognized submitted creature, #{previously_swallowed_creature}."    
+      raise "Unrecognized submitted creature. '#{previously_swallowed_creature}'."   
     end
     
-    @song_lyrics += line
+    @song_lyrics += output
     
     return previously_swallowed_creature
   end
   
   def display_song_lyrics
-    puts "\n\n#{@song_lyrics}"
+    puts "#{@song_lyrics}"
   end  
   
 end
